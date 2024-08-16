721a7782a7b64751b03d503ff7fd33a5211303cc
Bencephalon <benjung2514@gmail.com>
2024-08-16 15:43:13 -0400

    added_r_documentation

--- data-raw/create_ggseg_tissue_atlas_package.R
+++ data-raw/create_ggseg_tissue_atlas_package.R
@@ -9,15 +9,27 @@ aseg_all_rds <- "~/Documents/ggseg-tissueclass-atlas/aseg_all_atlas.RDS"
 aseg_all <- readRDS(aseg_all_rds)
 
 use_data(aseg_all, overwrite = T)
-document()
 
-# Save aseg_all to data
+# Save aseg_cerebrum to data
 aseg_cerebrum_rds <- "~/Documents/ggseg-tissueclass-atlas/aseg_cerebrum_atlas.RDS"
 aseg_cerebrum <- readRDS(aseg_cerebrum_rds)
 
 use_data(aseg_cerebrum, overwrite = T)
+
 document()
 
-# Copy remaining files into data-raw
+install()
 
-install("/mnt/isilon/bgdlab_processing/Ben/ggsegTissue")
+# Test
+library(ggsegTissue)
+help(aseg_all)
+
+#make dataframe
+df <- data.frame(my_variable = c(1, 3, 7, 4),
+                 tissue_class = c("cGM", "CSF", "sGM", "WM"))
+
+#plot
+ggplot(df) +
+  ggseg::geom_brain(atlas = aseg_cerebrum, 
+                    aes(fill = my_variable)) +
+  ggtitle("This is my brain plot!")
